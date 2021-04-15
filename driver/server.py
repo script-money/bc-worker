55d21c07edde5dfa9e9759a28a6d9c99841fc6fc
脚本刷钱 <hello@script.money>
2021-04-15 18:59:19 +0800

    :wheelchair: import server json structer

--- driver/server.py
+++ driver/server.py
@@ -10,26 +10,27 @@ socket = context.socket(zmq.PUSH)
 socket.connect('tcp://localhost:5557')
 
 
-@app.route('/buy', methods=['POST'])
-def send_buy_signal():
-    data = request.json  # type(data) is dict
-    msg = f"0 {data['username']} {data['usd']}"
-    print(f"recieve message: {msg}")
-    try:
-        socket.send_string(msg)
-    except queue.Full:
-        print('Task queue is full')
-    return 'success', 200
+@app.route('/', methods=['GET'])
+def hello_world():
+    return 'Hello, World!'
 
-
-@app.route('/follow_and_dm', methods=['POST'])
-def follow_and_dm_signal():
+@app.route('/', methods=['POST'])
+def send_signal():
     data = request.json  # type(data) is dict
-    msg = f"7 {data['username']}"
-    print(f"recieve message: {msg}")
-    try:
-        socket.send_string(msg)
-    except queue.Full:
-        print('Task queue is full')
-        return 'retry', 404
-    return 'success', 200
+    request_type = data['type']
+    if request_type == 'buy':
+        msg = f"0 {data['username']} {data['usd']}"
+        print(f"recieve message: {msg}")
+        try:
+            socket.send_string(msg)
+        except Exception as e:
+            return jsonify({"code": 404, "msg": e})
+        return jsonify({"code": 200, "msg": "buy success"})
+    elif request_type == 'follow_and_dm':
+        msg = f"7 {data['username']}"
+        print(f"recieve message: {msg}")
+        try:
+            socket.send_string(msg)
+        except Exception as e:
+            return jsonify({"code": 404, "msg": e})
+        return jsonify({"code": 200, "msg": "follow_and_dm success"})
